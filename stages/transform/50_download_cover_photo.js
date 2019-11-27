6164b8c40f7a9678ea42bdc846d754b0119b8c79
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-11-27 13:35:54 +0100

    Detect image filetype and set file extension

--- stages/transform/50_download_cover_photo.js
+++ stages/transform/50_download_cover_photo.js
@@ -1,4 +1,5 @@
-const { io, http } = require("lastejobb");
+const { image, io, http } = require("lastejobb");
+const fs = require("fs");
 
 let images = io.lesBuildfil("coverphoto").items;
 
@@ -7,7 +8,19 @@ for (var i = 0; i < 3; i++) downloadNext();
 async function downloadNext() {
   if (images.length <= 0) return;
   const { kode, value } = images.pop();
-  const fn = "build/" + kode + ".jpg";
-  if (!io.fileExists("temp/" + fn)) await http.downloadBinary(value, fn);
+  await downloadAndDetectFiletype(kode, value);
   downloadNext();
+}
+
+async function downloadAndDetectFiletype(kode, url) {
+  const jpegFn = "build/" + kode + ".jpg";
+  if (io.fileExists("temp/" + jpegFn)) return;
+  const pngFn = "build/" + kode + ".png";
+  if (io.fileExists("temp/" + pngFn)) return;
+  const tempFn = "build/" + kode + ".tmp";
+  await http.downloadBinary(url, tempFn);
+  const type = image.getFileType("temp/" + tempFn);
+  if (!type) log.warn("Unknown file type for " + tempFn);
+  const targetFn = "temp/build/" + kode + "." + type;
+  fs.renameSync("temp/" + tempFn, targetFn);
 }
